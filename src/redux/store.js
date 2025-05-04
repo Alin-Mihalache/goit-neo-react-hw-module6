36e977eca541eee0852980ae10a1d426a1860901
Alin-Mihalache <mihalache.alinbogdan@gmail.com>
2025-05-04 10:40:42 +0200

    commit 1

---
+++ src/redux/store.js
@@ -0,0 +1,31 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import contactsReducer from "./contactsSlice";
+import filtersReducer from "./filtersSlice";
+
+const store = configureStore({
+  reducer: {
+    contacts: persistReducer({ key: "contacts", storage }, contactsReducer),
+    filters: filtersReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
